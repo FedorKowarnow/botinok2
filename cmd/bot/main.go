753fad3f7954ea3bba2a51786eed6e559b3d7279
Fedor Kowarnow <poxxperias@mail.ru>
2024-05-29 13:27:17 +0300

    obnova3

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const token = "TOKEN"
 
 func main() {
-	fmt.Println("hello")
+	bot, err := tgbotapi.NewBotAPI("7348923803:AAEhFfw6rVZtceBTh7INvR__n5lFWLT5CIM")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/FedorKowarnow/botinok2
 
 go 1.21.4
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
